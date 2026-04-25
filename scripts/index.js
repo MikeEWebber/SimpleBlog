76e9e774e80bf498bf2a21b5bbb349d43e12b964
Mike Webber <mikewebber@hotmail.co.uk>
2026-04-25 11:22:19 +0100

    Addition of basic layout and styling

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,24 @@ let dataString = ``;
 // loop through all items in the blogEntries array and display
 
 blogEntries.forEach(element => {
-    dataString = dataString + `<p> ${element.title} </p><p>${element.summary} </p>`
+    dataString = dataString + 
+        `
+        <div class="blog-element">
+            <div class="header-container">
+                <p class="title-element"> ${element.title} </p>
+                <p class="spacer-element"></p>
+                <p class="date-element"> ${element.date}</p>
+                        
+            </div>
+                    
+            <p class="summary-element">${element.summary} </p>
+            <div class="btn-right">
+                <input type="button" class="read-more-button" value="Read More">
+            </div>
+
+            
+        </div>
+        `
 })
 
 
