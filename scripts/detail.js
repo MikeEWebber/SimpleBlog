9e87ef5aecd79d9bc1cfd61c03f0effcb4a3f7c3
Mike Webber <mikewebber@hotmail.co.uk>
2026-05-01 13:18:11 +0100

    Added detail to detail page - not styled yet

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,14 +1,21 @@
 import {queryParam} from '../utils/query-params.js';
+import {getBlogEntryByID} from '../data/blog_data.js';
 
 // get the URL of current page and get the query parameter.
 let pageID = queryParam(window.location.href, 'id');
 console.log("Param is: " + pageID); 
 
+let blogEntry = getBlogEntryByID(pageID);
 
+let dataString =  `
+                  <p class="title-element"> ${blogEntry.title} </p>
+                  <p class=""> ${blogEntry.date} </p>
+                  <p class=""> ${blogEntry.summary} </p>
+                  <p class=""> ${blogEntry.detail} </p>
 
+                  `;
 
-
-
+document.querySelector('.js-blog-body-container').innerHTML = dataString;
 
 document.querySelector('.js-return-button').addEventListener('click', () => {
   window.location.replace(`SimpleBlog/../index.html`);
